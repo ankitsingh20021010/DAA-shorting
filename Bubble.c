7efd495964f5b71c9e2af2f2b4af1d4eb266c7f8
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-02-24 11:18:15 +0530

    Implement insertion sort in Bubble.c
    Added insertion sort function to sort an array.

--- Bubble.c
+++ Bubble.c
@@ -31,6 +31,20 @@ void selectionshort(int arr[],int n){
         }
     }
 }
+
+//sortig
+void insertionsorting(int arr[],int n){
+    for(int i=1; i<n; i++){
+        int curr=arr[i];
+        int prev=i-1;
+        while(prev>=0 && arr[prev]>curr){
+            arr[prev+1]=arr[prev];
+            prev--;
+        }
+        arr[prev+1]=curr;
+}
+}
+
 int main() {
  int arr[]={4,1,5,2,3};
  int n=sizeof(arr)/sizeof(arr[0]);
